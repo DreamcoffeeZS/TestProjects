e4e6d6a5610397c4066d954afe0a0a3735c3e4db
zhoushuai <dreamcofeezs@163.com>
2021-10-25 00:12:21 +0800

    整理文件

---
+++ 04.动静态库实战/01.测试多架构合并/Archives/ZSTools/ZSTools/ZSObject.m
@@ -0,0 +1,16 @@
+//
+//  ZSObject.m
+//  ZSTools
+//
+//  Created by zhoushuai on 2021/10/24.
+//
+
+#import "ZSObject.h"
+
+@implementation ZSObject
+
+-(void)testPrint {
+	NSLog(@"[ZSTool] testPrint!!!");
+}
+
+@end
