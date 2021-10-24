f2dfb2f17bef56da3ec2c72d583bb0d6141e674f
zhoushuai <dreamcofeezs@163.com>
2021-10-25 00:14:34 +0800

    整理文件

---
+++ 04.动静态库使用/01.测试多架构合并/xcframework/ZSApp/ZSApp/ViewController.m
@@ -0,0 +1,19 @@
+//
+//  ViewController.m
+//  ZSApp
+//
+//  Created by zhoushuai on 2021/10/24.
+//
+
+#import "ViewController.h"
+#import <ZSTools/ZSTools.h>
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    ZSObject *object = [[ZSObject alloc] init];
+    [object testPrint];
+}
+
+@end
